43754f5ab24e9dc0ce7bf55a0342645c5302002f
Fyre <fyre.homelab+github@gmail.com>
2022-08-01 00:55:36 -0500

    Implement attacking, damage, and death.

--- README.md
+++ README.md
@@ -29,7 +29,7 @@ Main Tutorial:
 - ~~Part 3 - Generating a dungeon~~
 - ~~Part 4 - Field of view~~
 - ~~Part 5 - Placing enemies and kicking them (harmlessly)~~
-- Part 6 - Doing (and taking) some damage
+- ~~Part 6 - Doing (and taking) some damage~~
 - Part 7 - Creating the Interface
 - Part 8 - Items and Inventory
 - Part 9 - Ranged Scrolls and Targeting
--- entity.py
+++ entity.py
@@ -3,6 +3,8 @@ from __future__ import annotations
 import copy
 from typing import Optional, Tuple, Type, TypeVar, TYPE_CHECKING
 
+from render_order import RenderOrder
+
 if TYPE_CHECKING:
     from components.ai import BaseAI
     from components.fighter import Fighter
@@ -25,6 +27,7 @@ class Entity:
         colour: Tuple[int, int, int] = (255, 255, 255),
         name: str = "<Unnamed>",
         blocks_movement: bool = False,
+        render_order: RenderOrder = RenderOrder.CORPSE,
     ):
         self.x = x
         self.y = y
@@ -32,6 +35,7 @@ class Entity:
         self.colour = colour
         self.name = name
         self.blocks_movement = blocks_movement
+        self.render_order = render_order
         if gamemap:
             # If gamemap isn't provided now then it will be set later.
             self.gamemap = gamemap
@@ -80,6 +84,7 @@ class Actor(Entity):
             colour=colour,
             name=name,
             blocks_movement=True,
+            render_order=RenderOrder.ACTOR,
         )
 
         self.ai: Optional[BaseAI] = ai_cls(self)
