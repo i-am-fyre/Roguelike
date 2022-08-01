22fad525c4802862453dd88110aff19ba37ad624
Fyre <fyre.homelab+github@gmail.com>
2022-07-31 23:22:10 -0500

    Introducing enemies and contact.

--- README.md
+++ README.md
@@ -28,7 +28,7 @@ Main Tutorial:
 - ~~Part 2 - The generic Entity, the render functions, and the map~~
 - ~~Part 3 - Generating a dungeon~~
 - ~~Part 4 - Field of view~~
-- Part 5 - Placing enemies and kicking them (harmlessly)
+- ~~Part 5 - Placing enemies and kicking them (harmlessly)~~
 - Part 6 - Doing (and taking) some damage
 - Part 7 - Creating the Interface
 - Part 8 - Items and Inventory
--- procgen.py
+++ procgen.py
@@ -2,9 +2,11 @@ from __future__ import annotations
 
 import random
 from typing import Iterator, List, Tuple, TYPE_CHECKING
+from numpy import number
 
 import tcod
 
+import entity_factories
 from game_map import GameMap
 import tile_types
 
@@ -39,6 +41,21 @@ class RectangularRoom:
             and self.y2 >= other.y1
         )
 
+def place_entities(
+    room: RectangularRoom, dungeon: GameMap, maximum_monsters: int,
+) -> None:
+    number_of_monsters = random.randint(0, maximum_monsters)
+
+    for i in range(number_of_monsters):
+        x = random.randint(room.x1 + 1, room.x2 - 1)
+        y = random.randint(room.y1 + 1, room.y2 - 1)
+
+        if not any(entity.x == x and entity.y == y for entity in dungeon.entities):
+            if random.random() < 0.8:
+                entity_factories.orc.spawn(dungeon, x, y)
+            else:
+                entity_factories.troll.spawn(dungeon, x, y)
+
 def tunnel_between(
     start: Tuple[int, int], end: Tuple[int, int]
 ) -> Iterator[Tuple[int, int]]:
@@ -64,6 +81,7 @@ def generate_dungeon(
     room_max_size: int,
     map_width: int,
     map_height: int,
+    max_monsters_per_room: int,
     player: Entity,
 ) -> GameMap:
     """Generate a new dungeon map."""
@@ -96,6 +114,8 @@ def generate_dungeon(
             # Dig out a tunnel between this room and the previous one.
             for x, y in tunnel_between(rooms[-1].center, new_room.center):
                 dungeon.tiles[x, y] = tile_types.floor
+
+        place_entities(new_room, dungeon, max_monsters_per_room)
         
         # Finally, append the new room to the list.
         rooms.append(new_room)
