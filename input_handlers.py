34f7c0d19423b89d44aa96a797ea6ec00fa1c5c4
Fyre <fyre.homelab+github@gmail.com>
2022-08-06 00:38:00 -0500

    Add new consumable: Fireball Scroll.

--- input_handlers.py
+++ input_handlers.py
@@ -273,7 +273,7 @@ class LookHandler(SelectIndexHandler):
         self.engine.event_handler = MainGameEventHandler(self.engine)
 
 class SingleRangedAttackHandler(SelectIndexHandler):
-    """Handles targetting a single enemy. Only the enemy selected will be affected."""
+    """Handles targeting a single enemy. Only the enemy selected will be affected."""
 
     def __init__(
         self, engine: Engine, callback: Callable[[Tuple[int, int]], Optional[Action]]
@@ -282,6 +282,39 @@ class SingleRangedAttackHandler(SelectIndexHandler):
 
         self.callback = callback
 
+    def on_index_selected(self, x: int, y: int) -> Optional[Action]:
+        return self.callback((x, y))
+
+class AreaRangedAttackHandler(SelectIndexHandler):
+    """Handles targeting an area within a given radius. Any entity within the area will be affected."""
+
+    def __init__(
+        self, 
+        engine: Engine,
+        radius: int,
+        callback: Callable[[Tuple[int, int]], Optional[Actor]],
+    ):
+        super().__init__(engine)
+
+        self.radius = radius
+        self.callback = callback
+
+    def on_render(self, console: tcod.Console) -> None:
+        """Highlight the tile under the cursor."""
+        super().on_render(console)
+
+        x, y = self.engine.mouse_location
+
+        # Draw a rectangle around the targeted area, so the player can see the affected tiles.
+        console.draw_frame(
+            x=x - self.radius - 1,
+            y=y - self.radius - 1,
+            width=self.radius ** 2,
+            height=self.radius ** 2,
+            fg=colours.red,
+            clear=False,
+        )
+    
     def on_index_selected(self, x: int, y: int) -> Optional[Action]:
         return self.callback((x, y))
 
