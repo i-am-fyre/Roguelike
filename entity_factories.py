5eca14f23b176be1f5905f7216c5c57fd27438a4
Fyre <fyre.homelab+github@gmail.com>
2022-08-06 00:23:17 -0500

    Add new consumable: Confusion Scroll.

--- entity_factories.py
+++ entity_factories.py
@@ -43,3 +43,10 @@ lightning_scroll = Item(
     name = "Lightning Scroll",
     consumable=consumable.LightningDamageConsumable(damage=20, maximum_range=5),
 )
+
+confusion_scroll = Item(
+    char="~",
+    colour=(207, 63, 255),
+    name = "Confusion Scroll",
+    consumable=consumable.ConfusionConsumable(number_of_turns=10),
+)
